befdc95a97e8bd3cec1d25af1148c1808c2678c2
Harrison Conlin <me@harrisony.com>
2013-02-09 13:12:53 +1100

    Bump to 1.9.3-p385

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of ruby.
 
+## 0.1.1:
+
+* Bump to 1.9.3-p385
+
 ## 0.1.0:
 
 * Initial release of handrolled ruby. We started with rbenv but multiple rubies in prod just seems to always trip us up.
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mylescarrick@gmail"
 license          "MIT"
 description      "Installs/Configures ruby from source"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "ruby_build"
 depends "rbenv"
