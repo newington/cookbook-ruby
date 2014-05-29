4949a9e3c6250c63a94751c3277f61cb21712cef
Harrison Conlin <me@harrisony.com>
2014-05-29 12:40:05 +1000

    be more chef like

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,7 @@
 # Copyright 2013, Myles Carrick
 #
 #
-
-ruby_version = '2.1.1'
-ruby_version_concat = ruby_version.gsub(/-/,'')
+ruby_version_concat = node[:ruby][:version].gsub(/-/,'')
 
 # Prereqs without which we can't get off the ground
 %w(build-essential wget libyaml-dev zlib1g-dev libssl-dev libreadline6-dev libxml2-dev).each do |dep|
@@ -18,7 +16,7 @@ bash "ruby from source" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-#{ruby_version}.tar.gz
+  wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-#{node[:ruby][:version]}.tar.gz
   tar -xzf ruby-#{ruby_version}.tar.gz
   cd ruby-#{ruby_version} && ./configure --prefix=/usr/local && make && sudo make install
   EOH
